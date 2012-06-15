86322fe741fe41803fd971b7ae37fc4658670352
Alex Nichol <alexnichol@comcast.net>
2012-06-15 19:49:37 -0400

    Initial Commit

---
+++ setres/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  setres
+//
+//  Created by Alex Nichol on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
